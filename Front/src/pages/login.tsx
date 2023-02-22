c6bf47ac30529954b834ac3b66894c111889b848
phakin <oot.pakin@gmail.com>
2023-02-22 09:52:12 +0700

    create input

--- Front/src/pages/login.tsx
+++ Front/src/pages/login.tsx
@@ -1,14 +1,27 @@
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import { Login as LoginIcon } from '@mui/icons-material';
-
+import { TextField } from '@mui/material';
 import '../index.css';
+import React from 'react';
 function Login() {
+    const [value, setValue] = React.useState('');
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setValue(event.target.value);
+          };
   return (
     <><div>
     <h1>test</h1>
     </div>
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 100 }}>
+    <TextField
+      id="box-input"
+      label="Enter text here"
+      value={value}
+      onChange={handleChange}
+      variant="outlined"
+    />
       <Button variant='contained' sx={{ fontSize: 'large' }} >
         <LoginIcon sx={{ mr: 1 }} />
         Log in
