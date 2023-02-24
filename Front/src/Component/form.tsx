9b32d8dac4db5f216e5716ec6d784fee39fd8022
phakin <oot.pakin@gmail.com>
2023-02-24 21:07:50 +0700

    2

--- Front/src/Component/form.tsx
+++ Front/src/Component/form.tsx
@@ -16,7 +16,7 @@ function Form(){
         <Box sx={{position: 'absolute',width: 543,height: 79,left: 0,top: 210,background: '#D9D9D9'}}>hello</Box>
         <Box sx={{position: 'absolute',backgroundColor: '#A6A6A6',height: 300,top:500,padding: '10px',margin: '15px',display: 'flex',flexDirection: 'column',fontSize:100}}>
             menu
-            <Box sx={{background:'#A6A6A6',width: 466,height: 200,left: 0,top: 300}}>
+            <Box sx={{background:'#ffffff',width: 466,height: 200,left: 0,top: 300,border: 2}}>
             <Typography sx={{ fontSize: 30 ,color: 'black' }}>text</Typography>
             <Typography sx={{ fontSize: 30 ,color: 'black' }}>text</Typography>
             <Typography sx={{ fontSize: 30 ,color: 'black' }}>text</Typography></Box>
