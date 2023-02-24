358b14f921ef9f378a97ef1600eeccca2135d9ef
phakin <oot.pakin@gmail.com>
2023-02-24 21:18:13 +0700

    1

--- Front/src/Component/form.tsx
+++ Front/src/Component/form.tsx
@@ -12,16 +12,16 @@ function Form(){
         <Box sx={{ position: 'absolute',width: 1440,height: 205,background: '#721001'}}>
             <Typography sx={{ fontSize: 30 ,color: 'white' }}>text</Typography>
         </Box>
-        <Box   sx={{ background:'#A6A6A6',position:'absolute',width: 1137,height: 79,left: 545,top: 210}}>ผู้ใช้</Box>
+        <Box sx={{ background:'#A6A6A6',position:'absolute',width: 1137,height: 79,left: 545,top: 210}}>ผู้ใช้</Box>
         <Box sx={{position: 'absolute',width: 543,height: 79,left: 0,top: 210,background: '#D9D9D9'}}>hello</Box>
-        <Box sx={{position: 'absolute',backgroundColor: '#A6A6A6',height: 300,top:500,padding: '10px',margin: '15px',display: 'flex',flexDirection: 'column',fontSize:100}}>
+        <Box sx={{position: 'absolute',backgroundColor: '#A6A6A6',width: 500,height: 150,left:0,top:300,display: 'flex',flexDirection: 'column',fontSize:100}}>
             menu
-            <Box sx={{background:'#ffffff',width: 466,height: 200,left: 0,top: 300,border: 2}}>
+            </Box>
+            <Box sx={{position: 'absolute',background:'#ffffff',width: 500,height: 700,left: 0,top: 450,border: 2}}>
             <Typography sx={{ fontSize: 30 ,color: 'black' }}>text</Typography>
             <Typography sx={{ fontSize: 30 ,color: 'black' }}>text</Typography>
             <Typography sx={{ fontSize: 30 ,color: 'black' }}>text</Typography></Box>
-        </Box>
-
+        <Box sx={{position: 'absolute',backgroundColor: '#D9D9D9',height: 150,width:1000,top:300,left:500,display: 'flex',flexDirection: 'column',fontSize:100}}>แสดง</Box>
         </>
     )
 }
