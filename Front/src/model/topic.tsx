f0ec486c2358c7d7ceade4b275219f48fb86b133
phakin <oot.pakin@gmail.com>
2023-02-26 17:55:33 +0700

    11

--- Front/src/model/topic.tsx
+++ Front/src/model/topic.tsx
@@ -1,5 +1,12 @@
 export default interface topic{
     id: number
     topic: string
-    
+    description?:string
+    file1?:File
+    file2?:File
+    file3?:File
+    file4?:File
+    file5?:File
+    file6?:File
+    file7?:File
   }
