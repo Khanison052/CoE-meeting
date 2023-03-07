c1d734daad10f21da3e90f511cbea13b8d04e816
Khanison Buamiklin <6510110052@psu.ac.th>
2023-03-07 14:43:58 +0700

    fixed

--- Front/src/Component/form.tsx
+++ Front/src/Component/form.tsx
@@ -28,9 +28,8 @@ function Form(props: Prop) {
         <>
             <Box sx={{ position: 'absolute', width: '100%', height: '15%', background: '#721001', display: 'flex', gridTemplateColumns: 'auto 1fr', alignItems: 'center', gap: '20px' }}>
                 <img src={require("../images/logoengpsu.png")} className='image' />
-                <Typography sx={{ width: '100%', fontSize: 42.3, color: 'white' }}>เอกสารการประชุมคณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์</Typography>
+                <Typography sx={{ width: '100%', fontSize: 42.3, color: 'white', display: 'flex', flexDirection: 'column' }}>เอกสารการประชุมคณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์</Typography>
             </Box>
-
 
 
             <Box sx={{ background: '#A6A6A6', position: 'absolute', width: '50%', height: '7%', left: '30%', top: '15%', border: 1, display: 'flex' }}>
@@ -42,7 +41,7 @@ function Form(props: Prop) {
 
 
             {userRole && (
-                <Box sx={{ position: 'absolute', width: '30%', height: '7%', left: 0, top: '15%', background: '#D9D9D9', fontSize: 30, border: 1, textAlign: 'left' }}>
+                <Box sx={{ position: 'absolute', width: '30%', height: '7%', left: 0, top: '15%', background: '#D9D9D9', fontSize: 25, border: 1, textAlign: 'left' }}>
                     ผู้ใช้: {userRole.username}
                 </Box>
             )}
@@ -51,10 +50,10 @@ function Form(props: Prop) {
             <Box sx={{ position: 'absolute', backgroundColor: '#A6A6A6', width: '30%', height: '7%', left: 0, top: '22%', display: 'flex', flexDirection: 'column', fontSize: 30, border: 1, textAlign: 'center' }}>
                 รายการ
             </Box>
-            <Box sx={{ position: 'absolute', background: '#ffffff', width: '30%', height: '80%', left: 0, top: '29%', border: 1 }}>
-                <Typography sx={{ fontSize: 30, color: 'black', margin: 10 }} onClick={Today} >การประชุมวันนี้</Typography>
-                <Typography sx={{ fontSize: 30, color: 'black', margin: 10 }} onClick={All} >การประชุมทั้งหมด</Typography>
-                <Typography sx={{ fontSize: 30, color: 'black', margin: 10 }} onClick={Past} >การประชุมที่ผ่านมาแล้ว</Typography></Box>
+            <Box sx={{ position: 'absolute', background: '#ffffff', width: '30%', height: '100%', left: 0, top: '29%', border: 1, display: 'flex' , flexDirection: 'column' }}>
+                <Typography sx={{ fontSize: 27, color: 'black', margin: 10 }} onClick={Today} >การประชุมวันนี้</Typography>
+                <Typography sx={{ fontSize: 27, color: 'black', margin: 10 }} onClick={All} >การประชุมทั้งหมด</Typography>
+                <Typography sx={{ fontSize: 27, color: 'black', margin: 10 }} onClick={Past} >การประชุมที่ผ่านมาแล้ว</Typography></Box>
             <Box sx={{ position: 'absolute', backgroundColor: '#D9D9D9', height: '7%', width: '70%', top: '22%', left: '30.1%', display: 'flex', flexDirection: 'column', fontSize: 30, border: 1, textAlign: 'center' }}>แสดงรายละเอียดการประชุม</Box>
         </>
     )
