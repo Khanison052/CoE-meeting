45e71e9301659049cb4a34f90ca806be9cecf445
ชารีฟ สันติเพชร <hifrook.zen5@gmail.com>
2023-03-09 16:12:32 +0700

    แก้ไข usermanage Page

--- Front/src/pages/manageuser.tsx
+++ Front/src/pages/manageuser.tsx
@@ -62,9 +62,9 @@ function ManageUser() {
         <>
             <Box className={"title"}>สมาชิก
                 <Button sx={{ m: 2, float: 'right' }} variant="contained" onClick={() => setCreateFormPopup(true)}>
-                    <Add /> Announcements
+                    <Add /> เพิ่มสมาชิก
                 </Button>
-            </Box>
+            </Box><br />
             {UserRole.map((UserRole) => (
                 <Form userRole={UserRole}></Form>
             ))}
